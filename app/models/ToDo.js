0b1a1cf1a0b00b7e2ce1b10fe1786a4b188205a1
Kevin Koontz <contactkkoontz@gmail.com>
2024-09-06 16:16:49 -0600

    created todoHTMLTemplate

--- app/models/ToDo.js
+++ app/models/ToDo.js
@@ -11,12 +11,22 @@ export class ToDo {
 
   get quoteHTMLTemplate() {
     return /*html*/ `
-    <div class="fs-3">
+    <div>
+      <div class="fs-3">
         <span>"Would you like a cookie?"</span>
-    </div>
-    <div class="fs-6 text-end">
+      </div>
+      <div class="fs-6 text-end">
         <span>-The Oracle</span>
-    </div>
+      </div>
   `
+  }
+
+  get todoHTMLTemplate() {
+    return /*html*/ `
+    <div class="d-flex">
+        <span>></span>
+        <span>3 Todos remaining</span>
+    </div>
+    `
   }
 }
